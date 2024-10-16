099c595826816320a0687917273cac8e0a5f9882
Leo ARNAL-ANGER <leo.arnal@irit.fr>
2024-10-16 15:16:04 +0200

    Update Voxel Grid

--- src/VoxelGrid/voxelGridAdapter.h
+++ src/VoxelGrid/voxelGridAdapter.h
@@ -6,32 +6,50 @@
 #define VOXELGRIDADAPTER_H
 
 
+#include <Eigen/Geometry>
 #include <array>
 #include <memory>
-#include <Eigen/Geometry>
-#include "iVoxel.h"
 #include "Voxel.h"
 #include "VoxelGrid.h"
+#include "iVoxel.h"
 
 struct Quantity {
     int test = 0; // just for testing
 };
 
+template<typename Scalar, typename VectorType>
+std::pair<VectorType, VectorType> getMinMaxAsCube(const VectorType &min, const VectorType &max) {
+    Scalar width = (max.x() - min.x());
+    Scalar height = (max.y() - min.y());
+    Scalar depth = (max.z() - min.z());
+
+    Scalar maxDimension = std::max({width, height, depth});
+    Scalar dx = (maxDimension - width) / 2;
+    Scalar dy = (maxDimension - height) / 2;
+    Scalar dz = (maxDimension - depth) / 2;
+    VectorType min_bbox(min.x() - dx, min.y() - dy, min.z() - dz);
+    VectorType max_bbox(max.x() + dx, max.y() + dy, max.z() + dz);
+    return std::make_pair(min_bbox, max_bbox);
+}
+
 // Be carefull when using std::vector or Eigen::Matrix
-template <typename SampleMatrixType, typename VoxelGridType, typename DataType>
-void
-buildVoxelGrid (const SampleMatrixType& pos, const SampleMatrixType& norm, VoxelGridType& voxelGrid, int resolution, int N) {
+template<typename SampleMatrixType, typename VoxelGridType, typename DataType>
+void buildVoxelGrid(const SampleMatrixType &pos, const SampleMatrixType &norm, VoxelGridType &voxelGrid, int resolution,
+                    int N) {
     using VectorType = typename DataType::VectorType;
     using Aabb = typename VoxelGridType::Aabb;
+    using Scalar = typename DataType::Scalar;
 
     VectorType min_bbox, max_bbox;
-    Aabb bbox = Aabb(pos.colwise().minCoeff(), pos.colwise().maxCoeff());
+    min_bbox = pos.colwise().minCoeff();
+    max_bbox = pos.colwise().maxCoeff();
+    auto minmax = getMinMaxAsCube<Scalar, VectorType>(min_bbox, max_bbox);
+    Aabb bbox(minmax.first, minmax.second);
     voxelGrid.init(bbox, N, resolution);
     for (int i = 0; i < pos.rows(); i++) {
         DataType data(pos.row(i).transpose(), norm.row(i).transpose());
         voxelGrid.addData(data, i);
     }
-
 }
 
-#endif //VOXELGRIDADAPTER_H
+#endif // VOXELGRIDADAPTER_H
